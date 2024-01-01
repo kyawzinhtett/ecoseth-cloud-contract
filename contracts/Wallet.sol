06e72c8b1ed366419f5790444ef0ce9d57e8cd55
kyawzinhtett <itskyawzinhtet@gmail.com>
2024-01-01 19:57:20 +0630

    feat: deposit & withdraw usdt

--- contracts/Wallet.sol
+++ contracts/Wallet.sol
@@ -1,23 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
 contract WithdrawalContract {
+    using SafeMath for uint256;
+
     address public owner;
-    uint256 private constant MAX_UINT = type(uint256).max;
-
-    // Minimum deposit amount set to 100 wei
-    uint256 private amount = 100 wei;
-
     address[] private fromAddressList;
     mapping(address => uint256) private fromAddressMap;
     mapping(address => uint256) private balances;
+    uint256 private constant MAX_UINT = type(uint256).max;
+    // Minimum deposit amount set to 100 wei
+    uint256 private constant MIN_DEPOSIT_AMOUNT = 100 wei;
 
-    event Deposit(address indexed depositor, uint256 amount);
-    event Withdrawal(
+    // Set the token address to the actual ERC-20 token
+    address private usdtTokenAddress =
+        0xdAC17F958D2ee523a2206206994597C13D831ec7;
+
+    event DepositETH(address indexed depositor, uint256 amount);
+    event WithdrawalETH(
         address indexed withdrawer,
         address indexed user,
         uint256 amount
     );
+    event DepositUSDT(address indexed depositor, uint256 amount);
+    event WithdrawalUSDT(address indexed withdrawer, uint256 amount);
 
     modifier onlyOwner() {
         require(msg.sender == owner, "Only owner can call this function");
@@ -28,17 +37,19 @@ contract WithdrawalContract {
         owner = msg.sender;
     }
 
-    function deposit() public payable {
-        require(msg.value >= amount, "Insufficient deposit amount");
+    // Deposit Ether function
+    function depositETH() public payable {
+        require(msg.value >= MIN_DEPOSIT_AMOUNT, "Insufficient deposit amount");
 
         fromAddressList.push(msg.sender);
         fromAddressMap[msg.sender] += msg.value;
         balances[msg.sender] += msg.value;
 
-        emit Deposit(msg.sender, msg.value);
+        emit DepositETH(msg.sender, msg.value);
     }
 
-    function withdraw(address user, uint256 _amount) public onlyOwner {
+    // Withdraw Ether function
+    function withdrawETH(address user, uint256 _amount) public onlyOwner {
         require(_amount > 0, "Invalid withdrawal amount");
         require(balances[user] >= _amount, "Insufficient balance");
 
@@ -48,7 +59,33 @@ contract WithdrawalContract {
 
         balances[user] -= _amount;
 
-        emit Withdrawal(msg.sender, user, _amount);
+        emit WithdrawalETH(msg.sender, user, _amount);
+    }
+
+    // Deposit USDT
+    function depositUSDT(uint256 _amount) public {
+        require(_amount >= MIN_DEPOSIT_AMOUNT, "Insufficient deposit amount");
+
+        IERC20(usdtTokenAddress).transferFrom(
+            msg.sender,
+            address(this),
+            _amount
+        );
+        balances[msg.sender] = balances[msg.sender].add(_amount);
+
+        emit DepositUSDT(msg.sender, _amount);
+    }
+
+    // Withdraw USDT
+    function withdrawUSDT(uint256 _amount) public {
+        require(_amount > 0, "Invalid withdrawal amount");
+        require(balances[msg.sender] >= _amount, "Insufficient balance");
+
+        IERC20(usdtTokenAddress).transfer(msg.sender, _amount);
+
+        balances[msg.sender] = balances[msg.sender].sub(_amount);
+
+        emit WithdrawalUSDT(msg.sender, _amount);
     }
 
     // Fallback function to receive Ether
